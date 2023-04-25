25f14f38bdef788201fd1db65a8c12bc3edd47e1
d22isaka <d22isaka@student.his.se>
2023-04-25 15:50:15 +0200

    Made so the intent.putExtra  name and number gets the values of what the user puts in the fields in mainactivity

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,9 +19,16 @@ public class MainActivity extends AppCompatActivity {
         send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                EditText name = findViewById(R.id.namn);
+                String namn = name.getText().toString();
+
+                EditText size = findViewById(R.id.sko);
+                String number = size.getText().toString();
+
                 Intent intent = new Intent(MainActivity.this, ShowActivity.class);
-                intent.putExtra("name", "Daniel");
-                intent.putExtra("number", "42");
+                intent.putExtra("name",namn);
+                intent.putExtra("number", number);
                 startActivity(intent);
             }
         });
